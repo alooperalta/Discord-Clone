23c358c434c408eb9c61c4953bd85024dfe9eb6e
Kashish Jain <jainabhishek7204@gmail.com>
2021-01-21 10:24:11 +0530

    Firestore data was not rendering

--- src/Chat.js
+++ src/Chat.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import './Chat.css'
-import ChatHeader from './ChatHeader';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
-import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
-import GifIcon from '@material-ui/icons/Gif';
-import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
-import Message from './Message'
-import {useSelector} from 'react-redux';
-import {selectChannelId, selectChannelName} from './features/appSlice';
-import {selectUser} from './features/userSlice';
-import {useState} from 'react';
-import db from './firebase';
-import firebase from 'firebase';
-import {useEffect} from 'react';
-
+import React from "react";
+import "./Chat.css";
+import ChatHeader from "./ChatHeader";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
+import GifIcon from "@material-ui/icons/Gif";
+import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
+import Message from "./Message";
+import { useSelector } from "react-redux";
+import { selectChannelId, selectChannelName } from "./features/appSlice";
+import { selectUser } from "./features/userSlice";
+import { useState } from "react";
+import db from "./firebase";
+import firebase from "firebase";
+import { useEffect } from "react";
 
 function Chat() {
   const user = useSelector(selectUser);
   const channelId = useSelector(selectChannelId);
   const channelName = useSelector(selectChannelName);
-  const [input, setInput] = useState('');
-  const [messages, setMessages] =useState([]);
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     if (channelId) {
@@ -34,40 +33,49 @@ function Chat() {
 
   const sendMessage = (e) => {
     e.preventDefault();
-    db.collection('channels').doc(channelId).collection('messages').
-    add({
+    db.collection("channels").doc(channelId).collection("messages").add({
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
       message: input,
       user: user,
     });
-    setInput('');
+    setInput("");
   };
 
   return (
-    <div className = 'chat'>
-      <ChatHeader channelName/>
-      <div className = 'chat__messages'>
-      {messages.map((message) => (
-        <Message key={messages.user + message.timestamp} message={message} />
-      ))}
+    <div className="chat">
+      <ChatHeader channelName={channelName} />
+      <div className="chat__messages">
+        {messages.map((message) => (
+          <Message key={messages.user + message.timestamp} message={message} />
+        ))}
       </div>
 
-      <div className = 'chat__input'>
+      <div className="chat__input">
         <AddCircleIcon fontSize="large" />
         <form>
-          <input disabled = {!channelId} value = {input} onChange={(e) => setInput(e.target.value)} placeholder = {'Message #${channelName}'} />
-          <button onClick = {sendMessage} className = 'chat__inputButton' type= 'submit'>Send Message</button>
+          <input
+            disabled={!channelId}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder={`Message #${channelName}`}
+          />
+          <button
+            onClick={sendMessage}
+            className="chat__inputButton"
+            type="submit"
+          >
+            Send Message
+          </button>
         </form>
 
-        <div className = 'chat__inputIcons'>
-          <CardGiftcardIcon fontSize = 'large' />
-          <GifIcon fontSize = 'large' />
-          <EmojiEmotionsIcon fontSize = 'large' />
+        <div className="chat__inputIcons">
+          <CardGiftcardIcon fontSize="large" />
+          <GifIcon fontSize="large" />
+          <EmojiEmotionsIcon fontSize="large" />
         </div>
       </div>
     </div>
-
   );
 }
 
-export default Chat
+export default Chat;
